8e241c618df0385569d318218e20ece179589de5
Merwan Achibet <merwan@Merwan-desktop.(none)>
2011-11-18 20:01:50 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ TOMCAT_APPS=/var/lib/tomcat6/webapps
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo ln -s $DIR/Map/ $TOMCAT_APPS/Map
+sudo ln -s $DIR/Proxy/ $TOMCAT_APPS/Proxy
 sudo ln -s $DIR/RSS/ $TOMCAT_APPS/RSS
 sudo ln -s $DIR/Search/ $TOMCAT_APPS/Search
 sudo ln -s $DIR/Calendar/ $TOMCAT_APPS/Calendar
