8c2f093509e440e706ff3487a8d3fc39d03c0e12
Merwan Achibet <merwan@Merwan-desktop.(none)>
2011-11-19 12:24:56 +0100

    fixed calendar displaying wrong info

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -7,16 +7,20 @@ function Calendar(container, options) {
 
 	this.displayLength = 0;
 
+	// Données retournées par la reqûete AJAX.
 	this.events = null;
 
+	// On utilise Jquery Datepicker en tant que calendrier.
 	$.datepicker.setDefaults($.datepicker.regional['fr']);
 	this.calendar = this.container.datepicker({
 		beforeShowDay: this.markEvent.bind(this),
 		onChangeMonthYear: this.changeMonth.bind(this)
 	});
 
+	// List qui contiendra le détail des événements du mois.
 	this.list = $('<ul id="details"></ul>').appendTo(this.container);
 
+	// Chargement des données.
 	this.load();
 }
 
@@ -28,40 +32,51 @@ Calendar.prototype.load = function(year, month) {
 	// Ajoute une image de chargement.
 	this.container.prepend('<img src="loading.gif" class="loading" alt="Loading..."/>');
 
-	// Si aucune date n'est donnée en paramètre, on prend celle du jour.
+	// Si aucune date n'est précisée en paramètre, on prend celle du jour.
 	var today = year === undefined ? new Date() : new Date(year, month - 1, 1);
 
 	// Intervalle entre le premier et le dernier événement.
 	var firstDay = this.format(today);
 	var lastDay = this.format(new Date(today.getFullYear(), today.getMonth() + 1, 0));
 
-	var url = 'http://www.google.com/calendar/feeds/developer-calendar@google.com/public/full?strict=true&alt=json&singleevents=true&orderby=starttime&sortorder=a&start-min=' + firstDay + '&start-max=' + lastDay;
+	var url = 'http://www.google.com/calendar/feeds/developer-calendar@google.com/public/full?alt=json&singleevents=true&orderby=starttime&sortorder=a&start-min=' + firstDay + '&start-max=' + lastDay;
 
 	$.ajax({
 		url: '/Proxy/get?url=' + encodeURIComponent(url),
 		dataType: 'json',
 		success: this.record.bind(this),
-		error: function(xhr, status, err) { console.log('Erreur : impossible de récupérer le calendrier (', xhr.responseText, ')'); }
+		error: function(xhr, status, err) { console.log('Erreur : impossible de récupérer les données du calendrier (', xhr.responseText, ')'); }
 	});
 }
 
+/**
+ * Met en valeur le jour courant dans le calendrier si la
+ * date est enregistrée dans notre liste d'événements.
+ *
+ * La valeur retournée à JQuery doit être un tableau dont les
+ * valeur sont :
+ *
+ * - un booléen indiquant si la date est sélectionnable
+ * - la classe de la case courante
+ * - le contenu du tooltip apparaîssant au survol du curseur
+ */
 Calendar.prototype.markEvent = function(date) {
 
 	if(this.events === null)
 		return [false, '', ''];
 
+	// Convertit la date au format RFC3239 et vérifie si un événement
+	// est prévu à cette date.
 	var dateRFC3239 = this.format(date);
 	var event = this.events[dateRFC3239];
 
-	if(event) {
-
+	if(event)
 		return [true, 'important', event.title.$t];
-	}
 
 	return [false, '', ''];
 }
 
-Calendar.prototype.changeMonth = function(year, month, instance) {
+Calendar.prototype.changeMonth = function(year, month) {
 
 	$('#details').empty();
 	this.load(year, month);
@@ -77,16 +92,24 @@ Calendar.prototype.record = function(data) {
 
 	this.events = {};
 
+	// On enregistre les données dans un hash dont les identifiants
+	// sont les dates au format RFC3239 et dont les valeurs sont les
+	// objets JSON récupérés par notre requête.
 	data.feed.entry.forEach(function(item) {
-
 		var date = item.gd$when[0].startTime.substr(0, 10);
 		this.events[date] = item;
 	}.bind(this));
 
+	// Met à jour le calendrier et affiche les détails des événements.
 	this.container.datepicker('refresh');
 	this.display();
 }
 
+/**
+ * Prend en argument une Date javascript et retourne la châine de
+ * caractères la représentant au format RFC3239 utilisé par Google
+ * (YYYY-MM-DD).
+ */
 Calendar.prototype.format = function(date) {
 
 	var year = date.getFullYear();
@@ -105,10 +128,8 @@ Calendar.prototype.display = function() {
 
 	$.each(this.events, function(dateRFC3239, event) {
 
-		var date = new Date(Date.parse(event.gd$when[0].startTime));
-
 		var what = event.title.$t;
-		var when = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+		var when = dateRFC3239.substr(8, 2) + '/' + dateRFC3239.substr(5, 2) + '/' + dateRFC3239.substr(0, 4);
 		var where = event.gd$where[0].valueString;
 
 		var eventElement = $('<li></li>').appendTo(this.list);
